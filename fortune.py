a56d39e4bfc8115c42fff2c9e85c6719bc77615f
Christopher-John Doyle (CJ) <44242105+SouthPawCoder@users.noreply.github.com>
2022-01-11 16:50:20 -0500

    first commit

---
+++ fortune.py
@@ -0,0 +1,16 @@
+#fortune cookie program
+
+import random
+
+fortune = random.randit(0.4)
+
+if fortune == 0:
+    print("May you one day be carbon neutral")
+elif fortune == 1:
+    print("You have rice in your teeth")
+elif fortune == 2:
+    print("May the money in your life work for you")
+elif fortune == 3:
+    print("You will find love at your next turn")
+elif fortune == 4:
+    print("Fortune isn't here, maybe in your next cookie.")
